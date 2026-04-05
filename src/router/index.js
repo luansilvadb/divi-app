4e6157474997e3ad4d5b87b7863a62c4a4a0b3ed
luansilvadb <contato@luansilva.com.br>
2026-04-05 19:00:14 -0300

    build: remove all stale .js files causing Vite build resolution errors

--- src/router/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var vue_router_1 = require("vue-router");
-var router = (0, vue_router_1.createRouter)({
-    history: (0, vue_router_1.createWebHistory)(import.meta.env.BASE_URL),
-    routes: [],
-});
-exports.default = router;
--- src/shared/types/ui.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/**
- * Common UI-related types and interfaces.
- */
-Object.defineProperty(exports, "__esModule", { value: true });
